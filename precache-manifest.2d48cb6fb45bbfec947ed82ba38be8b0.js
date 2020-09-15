c5ff98ebc971e70f337b60ed3acce3fd987af588
rohit pandey <1080pandey@gmail.com>
2020-09-15 21:25:46 +0530

    Updates

---
+++ precache-manifest.2d48cb6fb45bbfec947ed82ba38be8b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31395d9e45c8b4b7cfaaf5a258100ef",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "9fb444db3743c04d59ee",
+    "url": "/movie-search/static/css/main.63613a1c.chunk.css"
+  },
+  {
+    "revision": "a9fb37a5f855adb0ce30",
+    "url": "/movie-search/static/js/2.38cc2916.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.38cc2916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb444db3743c04d59ee",
+    "url": "/movie-search/static/js/main.f6ef86a2.chunk.js"
+  },
+  {
+    "revision": "33c5120f9a4bc0cea922",
+    "url": "/movie-search/static/js/runtime-main.0bd1e727.js"
+  }
+]);
